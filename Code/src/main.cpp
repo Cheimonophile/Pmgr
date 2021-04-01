fcfee276dbe9230da66aa0f8c929055ef2037103
Benjamin McLemore <mclemorebenjamin@gmail.com>
2021-03-31 22:31:47 -0400

    Updated Makefile and Added Mainloop

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -3,38 +3,56 @@
 #include <iostream>
 #include <filesystem>
 #include <sqlite3.h>
+#include "util.h"
 #include "config.h"
 
+using namespace std;
 
 // main method
 int main(int argc, char* argv[]) {
 
     // get environment variables
-    std::string home_dir;
-    std::string config_dir;
-    std::string config_path;
+    string home_dir;
+    string config_dir;
+    string config_path;
     try {
-        home_dir = std::getenv("HOME");
+        home_dir = getenv("HOME");
         config_dir = home_dir + "/.config/passmgr";
         config_path = config_dir + "/passmgr.config";
     }
-    catch (const std::exception& e){
-        std::cout << e.what() << std::endl;
+    catch (const exception& e){
+        cout << e.what() << endl;
         return 0;
     }
 
     // make the directory
     system(("mkdir -p " + config_dir).c_str());
 
-    // make the config file
+    // make or read from the configurations file
     Config config(config_dir);
     config.configure();
 
     
+    // mainloop with buffer
+    cout << "pmgr> ";
+    string buffer;
+    string token;
+    while(getline(cin,buffer)) {
 
+        // read the first token
+        token = breakoff(buffer);
+
+        // if the token is exit or quit
+        if(token == "quit" || token == "exit") {
+            break;
+        }
+
+        // print the next prompt
+        cout << "pmgr> ";
+    }
 
 
     // exit code zero
-    std::cout << "Done!\n";
+    cout << "Done!\n";
     return 0;
 }
